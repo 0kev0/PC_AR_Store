a3da9bf9a3e19fdc40be0809206817c017bed45c
0kev0 <117107399+0kev0@users.noreply.github.com>
2025-05-13 12:16:03 -0600

    [HOT FIX/ ADD]
    - agregado dependencias y settings de gradle para incorporar arCore
    
    -recuperado archivos perdidos
    
    - eliminado directorios inecesarios
    
    - modificado manifest para incorporar requerimentos para ARcore
    
    -

---
+++ app/src/main/java/com/example/pcarstore/Activities/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.pcarstore.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.pcarstore.R;
+
+
+public class MainActivity extends AppCompatActivity {
+        private Button catalogo, loginGoogle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        catalogo = findViewById(R.id.ContinueWithoutAcount);
+        loginGoogle = findViewById(R.id.LoginGoogle);
+    }
+
+    public void VerCatologo(View view) {
+        Intent intent = new Intent(this, CatalogoActivity.class);
+        startActivity(intent);
+    }
+}
