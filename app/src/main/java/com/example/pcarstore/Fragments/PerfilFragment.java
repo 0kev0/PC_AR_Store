9fb640d48574e857254cbb2081738c06b7107377
0kev0 <117107399+0kev0@users.noreply.github.com>
2025-05-14 21:28:24 -0600

    [ADD] loading y estilos
    - agregado loading de lottie para la carga de imagenes
    
    - agregado estilo circle_image_view

--- app/src/main/java/com/example/pcarstore/Fragments/PerfilFragment.java
+++ app/src/main/java/com/example/pcarstore/Fragments/PerfilFragment.java
@@ -10,15 +10,8 @@ import android.view.ViewGroup;
 
 import com.example.pcarstore.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PerfilFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class PerfilFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class PerfilFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -29,16 +22,6 @@ public class PerfilFragment extends Fragment {
     public PerfilFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PerfilFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static PerfilFragment newInstance(String param1, String param2) {
         PerfilFragment fragment = new PerfilFragment();
         Bundle args = new Bundle();
